e276f2d62fc4fb82a2702ca3a0122c34e05434fd
Rock Baek <rbaek@splunk.com>
2019-10-16 13:48:27 -0700

    test

--- .circleci/deploy_connector.sh
+++ .circleci/deploy_connector.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -e
-echo "Setup kube client..."
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-sudo apt-get -y install gnupg
-sudo mkdir ~/.kube
-echo $GPG_KEY | gpg --output config --passphrase-fd 0 --decrypt --batch .circleci/kubeconfig.gpg
-sudo mv config ~/.kube/config
+# echo "Setup kube client..."
+# curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+# chmod +x ./kubectl
+# sudo mv ./kubectl /usr/local/bin/kubectl
+# sudo apt-get -y install gnupg
+# sudo mkdir ~/.kube
+# echo $GPG_KEY | gpg --output config --passphrase-fd 0 --decrypt --batch .circleci/kubeconfig.gpg
+# sudo mv config ~/.kube/config
 #Update helm server version
 helm init --force-upgrade
 #Make sure to check and clean previously failed deployment
